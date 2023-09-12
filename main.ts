eb2eabf281be48f79fd250bc27d09f9fba80fe99
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-09-12 08:08:45 -0700

    messsage

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
 import inquirer from "inquirer" 
+
+let answers = await inquirer.prompt ([
+    {
+        name: "userID",
+        type:"string",
+        message:"Please enter your ID: "
+    },
+    {
+        name: "userpin",
+        type:"number",
+        message:"Please enter your pin: "
+    },
+    {
+        name: "transactiontype",
+        type:"list",
+        choices: ["Fastcash","Withdrawl"],
+        message:"Please enter your Withdrawl Method: "
+    },
+    {
+        name: "withdrawl",
+        type:"list",
+        choices: [1000, 2000, 5000, 10000],
+        message:"Please enter your amount: "
+    },
+    {
+        name: "amount",
+        type:"number",
+        message:"Please enter your amount: "
+    },
+])
+
+
+
